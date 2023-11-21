c1be0c4110c0627215f0285db1bbc4eeaa7c0661
Josue Lemus <josueunueve@gmail.com>
2023-11-21 15:07:13 -0400

    Add shared preferences services

--- lib/config/services/api_services.dart
+++ lib/config/services/api_services.dart
@@ -1,18 +1,26 @@
 import 'dart:convert';
-import 'package:bomberos_ya/config/helpers/date_utils.dart';
-import 'package:bomberos_ya/config/helpers/db_helper.dart';
 import 'package:bomberos_ya/models/fire_types.dart';
 import 'package:http/http.dart' as http;
 
 class ApiServices {
   final baseUrl = "192.168.0.9:3001";
 
-  Future<List<FireTypes>> getFireTypes(String lasTimeModified) async {
-    final queryParameters = {'lastTimeModified': lasTimeModified};
-    final url = Uri.http(baseUrl, '/tipo-solicitudes', queryParameters);
-    final response = await http.get(url);
-    final json = jsonDecode(response.body);
-    return FireTypes.fromJsonList(json);
+  Future<List<FireTypes>> getFireTypes(String lastTimeModified) async {
+    try {
+      final queryParameters = {'lastTimeModified': lastTimeModified};
+      final url = Uri.http(baseUrl, '/tipo-solicitudes', queryParameters);
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final json = jsonDecode(response.body);
+        return FireTypes.fromJsonList(json);
+      } else {
+        throw Exception('Error en la solicitud HTTP: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error en la solicitud HTTP: $e');
+      throw Exception('Error en la solicitud HTTP');
+    }
   }
 
   Future<void> postReport(String audio, List<String> images) async {
