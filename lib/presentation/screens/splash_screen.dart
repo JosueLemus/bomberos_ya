2b476c568ea5fa8bc5579c3b508d053cc20fc1c6
Josue Lemus <josueunueve@gmail.com>
2023-11-29 11:09:07 -0400

    Update splash screen

--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:bomberos_ya/config/navigation/application_routes.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
+  static const name = 'splash-screen';
   const SplashScreen({super.key});
 
   @override
@@ -11,17 +12,43 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
+    super.initState();
     Future.delayed(Duration(seconds: 3), () {
       Navigator.pushReplacementNamed(context, Routes.home);
     });
-    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+
     return Scaffold(
       body: Container(
-        color: Colors.red,
+        // decoration: const BoxDecoration(
+        //     gradient: LinearGradient(
+        //         colors: [AppColors.primaryColor, AppColors.gradientColor],
+        //         begin: Alignment.topLeft,
+        //         end: Alignment.bottomRight)),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset("assets/bombero_icono.png"),
+                Text("BomberosYa!",
+                    style:
+                        textStyles.displaySmall!.copyWith(color: Colors.white)),
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: CircularProgressIndicator(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
